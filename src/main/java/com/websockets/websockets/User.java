75fd695d1fc1fd48bf1795aae6cd2c48080ecda0
Jacob Jansson <41388531+jaken1234@users.noreply.github.com>
2024-08-28 11:15:38 +0200

    docker

--- src/main/java/com/websockets/websockets/User.java
+++ src/main/java/com/websockets/websockets/User.java
@@ -2,12 +2,14 @@ package com.websockets.websockets;
 
 public class User {
     private String username;
+    private String password;
     private String userId;
 
     public User () {}
     
-    public User(String username, String userId) {
+    public User(String username, String password, String userId) {
         this.username = username;
+        this.password = password;
         this.userId = userId;
     }
 
@@ -17,6 +19,18 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 
     public String getUserId() {
