9cb22a359c49ce71844839121cfa667758bcf03f
Jacob Jansson <41388531+jaken1234@users.noreply.github.com>
2024-08-28 10:04:52 +0200

    login -> user

---
+++ src/main/java/com/websockets/websockets/User.java
@@ -0,0 +1,30 @@
+package com.websockets.websockets;
+
+public class User {
+    private String username;
+    private String userId;
+
+    public User () {}
+    
+    public User(String username, String userId) {
+        this.username = username;
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId() {
+        this.userId = userId;
+    }
+    
+}
