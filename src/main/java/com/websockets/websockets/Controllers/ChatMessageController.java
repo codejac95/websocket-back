1295318e13a3476d0d88cb6ebaa1d1c58201d719
Kribbz0r <kribbz0r@gmail.com>
2024-08-30 15:11:35 +0200

    Ellen needs fixes

--- src/main/java/com/websockets/websockets/Controllers/ChatMessageController.java
+++ src/main/java/com/websockets/websockets/Controllers/ChatMessageController.java
@@ -1,15 +1,15 @@
 package com.websockets.websockets.Controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.WebProperties.Resources.Chain;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.websockets.websockets.Services.ChatMessageService;
-import com.websockets.websockets.models.ChatRoom;
 import com.websockets.websockets.models.ChatMessage;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @CrossOrigin("*")
@@ -25,6 +25,11 @@ public class ChatMessageController {
     @PostMapping("/createChatMessage")
     public ChatMessage ChatMessage(@RequestBody ChatMessage chatMessage) {
         return chatMessageService.createChatMessage(chatMessage);
+    }
+
+    @GetMapping("/getRoomMessages")
+    public List<ChatMessage> ChatMessageList(@RequestBody String roomName) {
+        return chatMessageService.getRoomMessages(roomName);
     }
 
 }
--- src/main/java/com/websockets/websockets/Services/ChatMessageService.java
+++ src/main/java/com/websockets/websockets/Services/ChatMessageService.java
@@ -1,9 +1,9 @@
+
 package com.websockets.websockets.Services;
 
+import java.util.List;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Service;
-
-import com.websockets.websockets.models.ChatRoom;
 import com.websockets.websockets.models.ChatMessage;
 
 @Service
@@ -17,6 +17,16 @@ public class ChatMessageService {
 
     public ChatMessage createChatMessage(ChatMessage chatMessage) {
         return mongoOperations.save(chatMessage);
+    }
+
+    public List<ChatMessage> getRoomMessages(String roomName) {
+        // Query query= new Query();
+        // query.addCriteria(Criteria.where("chatRoom").is(roomName));
+
+        List<ChatMessage> chatMessages = mongoOperations.findAll(ChatMessage.class).stream()
+                .filter((ChatMessage chatMessage) -> chatMessage.getChatRoom().equals(roomName)).toList();
+        System.out.println(chatMessages);
+        return chatMessages;
     }
 
 }
